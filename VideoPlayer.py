280ea0bb392d7a0739894a8f16c02a7fb9c1b502
ayjays132 <Ayjays.ph@gmail.com>
2023-03-28 05:02:58 -0700

    Update VideoPlayer.py

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -12,158 +12,27 @@ import matplotlib.pyplot as plt
 
 from VideoPlayerLibrary import VideoPlayerLibrary
 
-class VideoPlayer(QMainWindow):
-    def __init__(self):
-        super().__init__()
+def open_file_dialog(self):
+    file_dialog = QFileDialog(self)
+    file_dialog.setFileMode(QFileDialog.ExistingFile)
+    file_dialog.setNameFilter("Video files (*.mp4 *.avi *.mov)")
+    if file_dialog.exec_():
+        file_paths = file_dialog.selectedFiles()
+        if file_paths:
+            self.open_video(file_paths[0])
 
-        self.setWindowTitle("Video Player")
-        self.resize(800, 600)
-
-        # Set up the video player library
-        self.video_player_library = None
-
-        # Set up the UI elements
-        self.video_label = QLabel()
-        self.setCentralWidget(self.video_label)
-
-        self.play_button = QPushButton("Play")
-        self.pause_button = QPushButton("Pause")
-        self.stop_button = QPushButton("Stop")
-        self.previous_button = QPushButton("Previous")
-        self.next_button = QPushButton("Next")
-
-        self.play_button.clicked.connect(self.play_video)
-        self.pause_button.clicked.connect(self.pause_video)
-        self.stop_button.clicked.connect(self.stop_video)
-        self.previous_button.clicked.connect(self.previous_frame)
-        self.next_button.clicked.connect(self.next_frame)
-
-        toolbar = self.addToolBar("Controls")
-        toolbar.addWidget(self.play_button)
-        toolbar.addWidget(self.pause_button)
-        toolbar.addWidget(self.stop_button)
-        toolbar.addWidget(self.previous_button)
-        toolbar.addWidget(self.next_button)
-
-        self.statusBar()
-
-        # Set up the timer
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-
-        # Set up the video playback state
-        self.playing = False
-        self.paused = False
-
-        # Set up the menu bar
-        menubar = self.menuBar()
-
-        # Set up the file menu
-        file_menu = menubar.addMenu("File")
-
-        open_file_action = QAction("Open File", self)
-        open_file_action.setShortcut("Ctrl+O")
-        open_file_action.triggered.connect(self.open_file)
-        file_menu.addAction(open_file_action)
-
-        close_file_action = QAction("Close File", self)
-        close_file_action.setShortcut("Ctrl+W")
-        close_file_action.triggered.connect(self.close_file)
-        file_menu.addAction(close_file_action)
-
-        file_menu.addSeparator()
-
-        exit_action = QAction("Exit", self)
-        exit_action.setShortcut("Ctrl+Q")
-        exit_action.triggered.connect(self.close)
-        file_menu.addAction(exit_action)
-
-        # Set up the playback menu
-        playback_menu = menubar.addMenu("Playback")
-
-        play_action = QAction("Play", self)
-        play_action.setShortcut("Space")
-        play_action.triggered.connect(self.play_video)
-        playback_menu.addAction(play_action)
-
-        pause_action = QAction("Pause", self)
-        pause_action.setShortcut("Space")
-        pause_action.triggered.connect(self.pause_video)
-        playback_menu.addAction(pause_action)
-
-        stop_action = QAction("Stop", self)
-        stop_action.setShortcut("Ctrl+Space")
-        stop_action.triggered.connect(self.stop_video)
-        playback_menu.addAction(stop_action)
-
-        playback_menu.addSeparator()
-
-        previous_frame_action = QAction("Previous Frame", self)
-        previous_frame_action.setShortcut("Left")
-        previous_frame_action.triggered.connect(self.previous_frame)
-        playback_menu.addAction(previous_frame_action)
-
-        next_frame_action = QAction("Next Frame", self)
-        next_frame_action.setShortcut("Right")
-        next_frame_action.triggered.connect(self.next_frame)
-        playback_menu.addAction(next_frame_action)
-
-        # Set up the filter menu
-        filter_menu = menu_bar.addMenu("Filter")
-
-        grayscale_filter_action = QAction("Grayscale", self)
-        grayscale_filter_action.triggered.connect(self.apply_grayscale_filter)
-
-        negative_filter_action = QAction("Negative", self)
-        negative_filter_action.triggered.connect(self.apply_negative_filter)
-
-        filter_menu.addAction(grayscale_filter_action)
-        filter_menu.addAction(negative_filter_action)
-
-        # Set up the histogram menu
-        histogram_menu = menu_bar.addMenu("Histogram")
-
-        frame_histogram_action = QAction("Frame Histogram", self)
-        frame_histogram_action.triggered.connect(self.show_frame_histogram)
-
-        video_histogram_action = QAction("Video Histogram", self)
-        video_histogram_action.triggered.connect(self.show_video_histogram)
-
-        histogram_menu.addAction(frame_histogram_action)
-        histogram_menu.addAction(video_histogram_action)
-
-        # Set up the playback slider
-        self.playback_slider = QSlider(Qt.Horizontal)
-        self.playback_slider.setFocusPolicy(Qt.NoFocus)
-        self.playback_slider.sliderMoved.connect(self.set_slider_position)
-
-        self.statusBar().addPermanentWidget(self.playback_slider)
-
-        # Set up the timer
-        self.timer = QTimer(self)
-        self.timer.setInterval(33)  # 30 frames per second
-        self.timer.timeout.connect(self.update_frame)
-
-        # Set up the video playback state
-        self.playing = False
-        self.paused = False
+def open_video(self):
+    file_path, _ = QFileDialog.getOpenFileName(self, "Select a Video File", "", "Video Files (*.mp4 *.avi)")
+    if file_path:
+        self.video_player_library = VideoPlayerLibrary(file_path)
         self.update_ui()
-
-    def open_file_dialog(self):
-        file_dialog = QFileDialog(self)
-        file_dialog.setFileMode(QFileDialog.ExistingFile)
-        file_dialog.setNameFilter("Video files (*.mp4 *.avi *.mov)")
-        if file_dialog.exec_():
-            file_paths = file_dialog.selectedFiles()
-            if file_paths:
-                self.open_video(file_paths[0])
-
-    def open_video(self, video_path):
-        self.video_player = VideoPlayerLibrary(video_path)
-        self.update_ui()
-        self.playback_slider.setRange(0, self.video_player.get_total_frames()-1)
+        self.playback_slider.setRange(0, self.video_player_library.get_total_frames() - 1)
         self.playback_slider.setValue(0)
         self.playback_slider.setEnabled(True)
+        self.statusBar().showMessage("Video loaded.")
+        self.play_video()
+        self.show()
+
 
     def close_video(self):
         self.video_player = None
@@ -317,3 +186,148 @@ def set_contrast(self, contrast):
 
             self.video_player.apply_filter(saturation_filter)
             self.update_ui()
+
+class VideoPlayer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Video Player")
+        self.resize(800, 600)
+
+        # Set up the video player library
+        self.video_player_library = None
+
+        # Set up the UI elements
+        self.video_label = QLabel()
+        self.setCentralWidget(self.video_label)
+
+        self.play_button = QPushButton("Play")
+        self.pause_button = QPushButton("Pause")
+        self.stop_button = QPushButton("Stop")
+        self.previous_button = QPushButton("Previous")
+        self.next_button = QPushButton("Next")
+
+        self.play_button.clicked.connect(self.play_video)
+        self.pause_button.clicked.connect(self.pause_video)
+        self.stop_button.clicked.connect(self.stop_video)
+        self.previous_button.clicked.connect(self.previous_frame)
+        self.next_button.clicked.connect(self.next_frame)
+
+        toolbar = self.addToolBar("Controls")
+        toolbar.addWidget(self.play_button)
+        toolbar.addWidget(self.pause_button)
+        toolbar.addWidget(self.stop_button)
+        toolbar.addWidget(self.previous_button)
+        toolbar.addWidget(self.next_button)
+
+        self.statusBar()
+
+        # Set up the timer
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+
+        # Set up the video playback state
+        self.playing = False
+        self.paused = False
+
+        # Set up the menu bar
+        menubar = self.menuBar()
+
+        # Set up the file menu
+        file_menu = menubar.addMenu("File")
+
+        open_file_action = QAction("Open File", self)
+        open_file_action.setShortcut("Ctrl+O")
+        open_file_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_file_action)
+
+        close_file_action = QAction("Close File", self)
+        close_file_action.setShortcut("Ctrl+W")
+        close_file_action.triggered.connect(self.close_file)
+        file_menu.addAction(close_file_action)
+
+        file_menu.addSeparator()
+
+        exit_action = QAction("Exit", self)
+        exit_action.setShortcut("Ctrl+Q")
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
+
+        # Set up the playback menu
+        playback_menu = menubar.addMenu("Playback")
+
+        play_action = QAction("Play", self)
+        play_action.setShortcut("Space")
+        play_action.triggered.connect(self.play_video)
+        playback_menu.addAction(play_action)
+
+        pause_action = QAction("Pause", self)
+        pause_action.setShortcut("Space")
+        pause_action.triggered.connect(self.pause_video)
+        playback_menu.addAction(pause_action)
+
+        stop_action = QAction("Stop", self)
+        stop_action.setShortcut("Ctrl+Space")
+        stop_action.triggered.connect(self.stop_video)
+        playback_menu.addAction(stop_action)
+
+        playback_menu.addSeparator()
+
+        previous_frame_action = QAction("Previous Frame", self)
+        previous_frame_action.setShortcut("Left")
+        previous_frame_action.triggered.connect(self.previous_frame)
+        playback_menu.addAction(previous_frame_action)
+
+        next_frame_action = QAction("Next Frame", self)
+        next_frame_action.setShortcut("Right")
+        next_frame_action.triggered.connect(self.next_frame)
+        playback_menu.addAction(next_frame_action)
+
+        # Set up the filter menu
+        filter_menu = menu_bar.addMenu("Filter")
+
+        grayscale_filter_action = QAction("Grayscale", self)
+        grayscale_filter_action.triggered.connect(self.apply_grayscale_filter)
+
+        negative_filter_action = QAction("Negative", self)
+        negative_filter_action.triggered.connect(self.apply_negative_filter)
+
+        filter_menu.addAction(grayscale_filter_action)
+        filter_menu.addAction(negative_filter_action)
+
+        # Set up the histogram menu
+        histogram_menu = menu_bar.addMenu("Histogram")
+
+        frame_histogram_action = QAction("Frame Histogram", self)
+        frame_histogram_action.triggered.connect(self.show_frame_histogram)
+
+        video_histogram_action = QAction("Video Histogram", self)
+        video_histogram_action.triggered.connect(self.show_video_histogram)
+
+        histogram_menu.addAction(frame_histogram_action)
+        histogram_menu.addAction(video_histogram_action)
+
+        # Set up the playback slider
+        self.playback_slider = QSlider(Qt.Horizontal)
+        self.playback_slider.setFocusPolicy(Qt.NoFocus)
+        self.playback_slider.sliderMoved.connect(self.set_slider_position)
+
+        self.statusBar().addPermanentWidget(self.playback_slider)
+
+        # Set up the timer
+        self.timer = QTimer(self)
+        self.timer.setInterval(33)  # 30 frames per second
+        self.timer.timeout.connect(self.update_frame)
+
+        # Set up the video playback state
+        self.playing = False
+        self.paused = False
+        self.update_ui()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    player = VideoPlayer()
+    player.show()
+    sys.exit(app.exec_())
+
